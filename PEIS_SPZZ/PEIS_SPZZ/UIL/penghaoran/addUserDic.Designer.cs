5d499d353a3c61e0e28217131a0a72d4b5ea9be9
SueGary <college.sujiali@gmail.com>
2013-05-31 22:43:59 +0800

    5.31彭浩然

---
+++ PEIS_SPZZ/PEIS_SPZZ/UIL/penghaoran/addUserDic.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace PEIS_SPZZ.UIL.penghaoran
+{
+    partial class addUserDic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.xhbhlab = new System.Windows.Forms.Label();
+            this.yhxmlab = new System.Windows.Forms.Label();
+            this.yhmlab = new System.Windows.Forms.Label();
+            this.mmlab = new System.Windows.Forms.Label();
+            this.tjkslab = new System.Windows.Forms.Label();
+            this.ksbmlab = new System.Windows.Forms.Label();
+            this.yhqxlab = new System.Windows.Forms.Label();
+            this.tishilab = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.qxbmlab = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Controls.Add(this.qxbmlab);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.tishilab);
+            this.groupBox1.Controls.Add(this.yhqxlab);
+            this.groupBox1.Controls.Add(this.ksbmlab);
+            this.groupBox1.Controls.Add(this.tjkslab);
+            this.groupBox1.Controls.Add(this.mmlab);
+            this.groupBox1.Controls.Add(this.yhmlab);
+            this.groupBox1.Controls.Add(this.yhxmlab);
+            this.groupBox1.Controls.Add(this.xhbhlab);
+            this.groupBox1.Location = new System.Drawing.Point(2, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(461, 279);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // xhbhlab
+            // 
+            this.xhbhlab.AutoSize = true;
+            this.xhbhlab.Location = new System.Drawing.Point(24, 39);
+            this.xhbhlab.Name = "xhbhlab";
+            this.xhbhlab.Size = new System.Drawing.Size(67, 15);
+            this.xhbhlab.TabIndex = 0;
+            this.xhbhlab.Text = "用户编号";
+            // 
+            // yhxmlab
+            // 
+            this.yhxmlab.AutoSize = true;
+            this.yhxmlab.Location = new System.Drawing.Point(241, 39);
+            this.yhxmlab.Name = "yhxmlab";
+            this.yhxmlab.Size = new System.Drawing.Size(67, 15);
+            this.yhxmlab.TabIndex = 1;
+            this.yhxmlab.Text = "用户姓名";
+            // 
+            // yhmlab
+            // 
+            this.yhmlab.AutoSize = true;
+            this.yhmlab.Location = new System.Drawing.Point(24, 93);
+            this.yhmlab.Name = "yhmlab";
+            this.yhmlab.Size = new System.Drawing.Size(52, 15);
+            this.yhmlab.TabIndex = 2;
+            this.yhmlab.Text = "用户名";
+            // 
+            // mmlab
+            // 
+            this.mmlab.AutoSize = true;
+            this.mmlab.Location = new System.Drawing.Point(255, 83);
+            this.mmlab.Name = "mmlab";
+            this.mmlab.Size = new System.Drawing.Size(37, 15);
+            this.mmlab.TabIndex = 3;
+            this.mmlab.Text = "密码";
+            // 
+            // tjkslab
+            // 
+            this.tjkslab.AutoSize = true;
+            this.tjkslab.Location = new System.Drawing.Point(24, 144);
+            this.tjkslab.Name = "tjkslab";
+            this.tjkslab.Size = new System.Drawing.Size(67, 15);
+            this.tjkslab.TabIndex = 4;
+            this.tjkslab.Text = "体检科室";
+            // 
+            // ksbmlab
+            // 
+            this.ksbmlab.AutoSize = true;
+            this.ksbmlab.Location = new System.Drawing.Point(241, 137);
+            this.ksbmlab.Name = "ksbmlab";
+            this.ksbmlab.Size = new System.Drawing.Size(67, 15);
+            this.ksbmlab.TabIndex = 5;
+            this.ksbmlab.Text = "科室编码";
+            // 
+            // yhqxlab
+            // 
+            this.yhqxlab.AutoSize = true;
+            this.yhqxlab.Location = new System.Drawing.Point(24, 191);
+            this.yhqxlab.Name = "yhqxlab";
+            this.yhqxlab.Size = new System.Drawing.Size(67, 15);
+            this.yhqxlab.TabIndex = 6;
+            this.yhqxlab.Text = "用户权限";
+            // 
+            // tishilab
+            // 
+            this.tishilab.AutoSize = true;
+            this.tishilab.ForeColor = System.Drawing.Color.Red;
+            this.tishilab.Location = new System.Drawing.Point(24, 243);
+            this.tishilab.Name = "tishilab";
+            this.tishilab.Size = new System.Drawing.Size(202, 15);
+            this.tishilab.TabIndex = 7;
+            this.tishilab.Text = "提示：各项信息都必须填完整";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(314, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(110, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 25);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(314, 80);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 25);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(110, 134);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 25);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(314, 134);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 25);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(110, 188);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 25);
+            this.textBox7.TabIndex = 14;
+            // 
+            // qxbmlab
+            // 
+            this.qxbmlab.AutoSize = true;
+            this.qxbmlab.Location = new System.Drawing.Point(244, 188);
+            this.qxbmlab.Name = "qxbmlab";
+            this.qxbmlab.Size = new System.Drawing.Size(67, 15);
+            this.qxbmlab.TabIndex = 15;
+            this.qxbmlab.Text = "权限编码";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(314, 181);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 25);
+            this.textBox8.TabIndex = 16;
+            // 
+            // addUserDic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 362);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "addUserDic";
+            this.Text = "添加用户";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label tishilab;
+        private System.Windows.Forms.Label yhqxlab;
+        private System.Windows.Forms.Label ksbmlab;
+        private System.Windows.Forms.Label tjkslab;
+        private System.Windows.Forms.Label mmlab;
+        private System.Windows.Forms.Label yhmlab;
+        private System.Windows.Forms.Label yhxmlab;
+        private System.Windows.Forms.Label xhbhlab;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label qxbmlab;
+    }
+}
