d62dd62d8201a82605d8631a07a2cfee08f297a3
SueGary <college.sujiali@gmail.com>
2013-06-01 15:40:32 +0800

    6.1彭浩然增加数据库连接类DBConnection.cs

---
+++ PEIS_SPZZ/PEIS_SPZZ/DBConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace PEIS_SPZZ
+{
+    class DBConnection
+    {
+        public static string sql = @"Data Source=PENGHAORAN-PC\MYDATABASE;Initial Catalog=tj_db;Integrated Security=True";
+        public static SqlConnection DtPh = new SqlConnection(sql);
+    }
+}
