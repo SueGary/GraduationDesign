39d11de6b6e94f09c800e6945b906e5c7f2c0551
SueGary <college.sujiali@gmail.com>
2013-06-01 13:11:30 +0800

    6.1

--- PEIS_SPZZ/PEIS_SPZZ/UIL/penghaoran/addUserDic.cs
+++ PEIS_SPZZ/PEIS_SPZZ/UIL/penghaoran/addUserDic.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PEIS_SPZZ.UIL.penghaoran
 {
@@ -15,7 +20,31 @@ namespace PEIS_SPZZ.UIL.penghaoran
         {
             InitializeComponent();
         }
-       
+        public bool seleUserDic()
+        {
+            bool validateUser = false;
+            
+            try
+            {                
+                string selectSql = string.Format("select count(*) from TJ_YHB where YHID = '{0}' or YHM = '{1}'", textBox1.Text, textBox3.Text);
+                SqlCommand sqlSelectCmd = new SqlCommand(selectSql);
+                int i = (int)sqlSelectCmd.ExecuteScalar();
+                if (i > 0)
+                {
+                    validateUser = true;
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+            	MessageBox.Show("错误：" + ex.Message,"提示",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            finally
+            {
+                  
+            }
+            return validateUser;
+        }
         private void addbtn_Click(object sender, EventArgs e)
         {
             if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "" && textBox7.Text != "" && textBox8.Text != "")
